da1e643ded961a18dc70c2eaffc26383e5e646c8
TamirhaG <96215456+TamirhaG@users.noreply.github.com>
2022-03-11 00:57:11 -0500

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by
-        <a href="https://www.linkedin.com/in/tamirha/" target="_blank">
-          Tamirha Giraldo
-        </a>
-        {""} and is {""}
-        <a href="https://github.com/TamirhaG/react-weather-app" target="_blank">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by
+          <a href="https://www.linkedin.com/in/tamirha/" target="_blank">
+            Tamirha Giraldo
+          </a>
+          {""} and is {""}
+          <a
+            href="https://github.com/TamirhaG/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
